0a2e7784dfebe9857e7bafb96f1ca546549e212b
Srihari Sriraman <srihari@nilenso.com>
2016-03-01 20:34:10 +0530

    Add yesterday's and today's notes
    - These aren't super neat just yet.

--- scratch.hs
+++ scratch.hs
@@ -36,3 +36,51 @@ addOneSquare x = y * y
 -- bottom-top style
 addOneSquare' x =  let y = x + 1
                     in y * y
+
+data User = NormalUser {  userName :: String
+                        , userAge :: Int}
+          | Admin {  userName :: String
+	           , userAge :: Int
+		   , adminRole :: String}
+	  deriving (Show)
+
+-- showUser :: User -> String
+-- showUser :: (NormalUser name age) =
+--   ("name is: " ++ name ++ " and age is:" ++ show age)
+-- showUser :: (Admin name age role) =
+--   ("name is: " ++ name ++ " and age is:" ++ (show age) ++ " role is: " ++ role)
+
+
+
+-- data IntList = EmptyList | List Int IntList
+-- let myl = List 1 (List 2 EmptyList)
+-- iLength IntList l = case l of {EmptyList -> 0; List i rest -> 1 + iLength rest}
+
+data BST = EmptyNode | Node {left::BST, value::Int, right::BST} deriving (Show)
+
+find :: BST -> Int -> Bool
+find EmptyNode _ = False
+find (Node l v r) x
+ | x == v = True
+ | x < v = find l x
+ | x > v = find r x
+
+insert :: BST -> Int -> BST
+insert EmptyNode x = Node EmptyNode x EmptyNode
+insert n@(Node l v r) x
+ | x == v = n
+ | x < v = Node (insert l x) v r
+ | x > v = Node l v (insert r x)
+
+fromList :: [Int] -> BST
+fromList [] = EmptyNode
+fromList (f:r) = insert (fromList r) f
+
+isBST :: BST -> Bool
+isBst EmptyNode = True
+isBST (Node l v r) = value l < v && value r > v
+
+-- test #1
+-- quickCheck (\list val -> let tree = fromList list in find (insert tree val) val)
+-- quickCheck (\list -> Data.List.sort list == Data.List.nub (inOrder (fromList list)))
+-- quickCheck (\list -> isBST (fromList list))
